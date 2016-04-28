b6dc55cc1f2ca4576ec578a17826fbb36b9db5d9
nilox <gabrielchicafernandez@gmail.com>
2016-04-28 14:36:33 +0200

    Training finished and validation integrated

--- 00 Development/processer.py
+++ 00 Development/processer.py
@@ -27,32 +27,47 @@ def printErrorMsg(text):
 #Given a Path, converts all images to grey scale and returns a list of Image objects
 def loadImgs(path):
 	return [Image(cv2.imread(join(path, f),0), None, None) for f in listdir(path) if (isfile(join(path, f)) and f.endswith('.jpg'))]
+#Given the keypoints of an image and the matrix of descriptors with matches return the list of KeyPoints associated
+def getKeyPointsFromDescriptor(keypoints, descriptor_matches):
+	kp = []
+	k = len(descriptor_matches[0])
+	for match in descriptor_matches:
+		for i in (0,k-1):
+			kp.append(keypoints[match[i].queryIdx]) #queryIdx = Index of the KeyPoint with match
+	return kp
+
 
 ######################################
 #			    MAIN			 	 #
 ######################################
 
 ####### Param specification #######
-debug = 1
-USE = "Use: <Script Name> <Training Dir> <Validation Dir>"
-if len(argv) < 2: # < 3 when implemented validation
+USE = "Use: <Script Name> <Training Dir> <Test Dir>"
+if len(argv) < 3: # < 3 when implemented validation
 	printErrorMsg("Param number incorrect\n"+USE)
 	exit(1)
 tPath = argv[1]
-#vPath = argv[2]
+vPath = argv[2]
 if not isdir(tPath):
 	printErrorMsg("'"+tPath+"'"+" is not a valid directory\n"+USE)
 	exit(1)
-#if (not isdir(vPath)):
-#	printErrorMsg("'"+tPath+"'"+" is not a valid directory\n"+USE)
-#	exit(1)
+if (not isdir(vPath)):
+	printErrorMsg("'"+tPath+"'"+" is not a valid directory\n"+USE)
+	exit(1)
 
 tImages = loadImgs(tPath)
-#vImages = loadImgs(vPath)
+vImages = loadImgs(vPath)
 
 #####KEYPOINTS CAPTURE#####
 orb = cv2.ORB(100,4,1)
+desc = [] #Training descriptors
 
+# Get keypoints and descriptors for each image
+for img in tImages:
+	#find and compute the keypoints with ORB
+	img.k, img.d = orb.detectAndCompute(img.img, None)
+	desc.append(img.d)
+	
 # FLANN parameters
 FLANN_INDEX_LSH = 6
 index_params= dict(algorithm = FLANN_INDEX_LSH,
@@ -63,16 +78,23 @@ search_params = dict(checks=50)   # or pass empty dictionary
 
 flann = cv2.FlannBasedMatcher(index_params,search_params)
 
-# Get keypoints and descriptors for each image
-for img in tImages:
-	#print "{0}/{1}".format(x+1,l)
-	#find and compute the keypoints with ORB
-	img.k, img.d = orb.detectAndCompute(img.img, None)
-	flann.add(img.d) #Adds the descriptor to the matcher
-
-## EXAMPLE ##
-matches = flann.match(tImages[0].d,tImages[1].d)
-img3 = drawMatches(tImages[0].img,tImages[0].k,tImages[1].img,tImages[1].k,matches[:10])
-plt.imshow(img3,),plt.show()
+## EXAMPLE ## This is for seeing the relationship between images
+#matches = flann.match(tImages[0].d,tImages[1].d)
+#img3 = drawMatches(tImages[0].img,tImages[0].k,tImages[1].img,tImages[1].k,matches[:10])
+#plt.imshow(img3,),plt.show()
 #############
+
+flann.add(desc)
+flann.train()
+
+print "FlannBasedMatcher succesfully trained"
+
+for imgV in vImages:
+	imgV.k, imgV.d = orb.detectAndCompute(imgV.img, None)
+	matchedDescriptors = flann.knnMatch(imgV.d, k=2)
+	img = cv2.drawKeypoints(imgV.img, getKeyPointsFromDescriptor(imgV.k, matchedDescriptors))
+
+	cv2.imshow('Matched Features', img)
+	cv2.waitKey(500) #500ms
+	cv2.destroyWindow('Matched Features')
 
